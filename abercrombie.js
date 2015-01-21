b024f06122e64eb4ea57b448cfd87a77d1d7546d
David Robbins <robbins.david@gmail.com>
2015-01-21 15:10:29 -0600

    Added place probe functionality

--- abercrombie.js
+++ abercrombie.js
@@ -1,5 +1,5 @@
 (function() {
-  var id, menu, name, version;
+  var abercrombie, id, menu, name, version;
 
   version = "0.0.1";
 
@@ -7,11 +7,50 @@
 
   imagejs.msg("" + id + " version " + version + " loaded.");
 
+  abercrombie = imagejs.modules[id] = {
+    alignCanvases: function() {
+      var cvBase, cvTop;
+      cvTop = abercrombie.getCanvas();
+      cvBase = document.getElementById("cvBase");
+      cvTop.style.left = cvBase.offsetLeft;
+      return cvTop.style.top = cvBase.offsetTop;
+    },
+    getCanvas: function() {
+      return document.getElementById("cvTop");
+    },
+    getContext: function() {
+      return abercrombie.getCanvas().getContext("2d");
+    },
+    paintProbe: function(x, y) {
+      var ctx;
+      ctx = abercrombie.getContext();
+      return ctx.strokeRect(x, y, 50, 50);
+    },
+    placeProbe: function() {
+      var cv;
+      cv = abercrombie.getCanvas();
+      cv.style.cursor = "crosshair";
+      abercrombie.alignCanvases();
+      return cv.onclick = function(evt, x, y) {
+        if (!x) {
+          x = evt.clientX - evt.target.offsetLeft + window.pageXOffset;
+        }
+        if (!y) {
+          y = evt.clientY - evt.target.offsetTop + window.pageYOffset;
+        }
+        return abercrombie.paintProbe(x, y);
+      };
+    }
+  };
+
   name = "Abercrombie (" + version + ")";
 
   menu = {
     "Start": function() {
       return imagejs.msg("You started something good.");
+    },
+    "Place Probe": function() {
+      return abercrombie.placeProbe();
     }
   };
 
--- src/abercrombie.coffee
+++ src/abercrombie.coffee
@@ -5,8 +5,32 @@ id      = "abercrombie"
 # Say Hello
 imagejs.msg "#{id} version #{version} loaded."
 
+
+# The magic (tm) happens here
+abercrombie = imagejs.modules[id] =
+    alignCanvases: ->
+        cvTop = abercrombie.getCanvas()
+        cvBase = document.getElementById("cvBase")
+        cvTop.style.left=cvBase.offsetLeft;
+        cvTop.style.top=cvBase.offsetTop;
+    getCanvas: -> document.getElementById("cvTop")
+    getContext: -> abercrombie.getCanvas().getContext("2d")
+    paintProbe: (x,y) ->
+        ctx = abercrombie.getContext()
+        ctx.strokeRect x, y, 50, 50
+    placeProbe: ->
+        cv = abercrombie.getCanvas()
+        cv.style.cursor = "crosshair"
+        abercrombie.alignCanvases()
+        cv.onclick = (evt,x,y) ->
+            if not x then x = evt.clientX-evt.target.offsetLeft+window.pageXOffset
+            if not y then y = evt.clientY-evt.target.offsetTop+window.pageYOffset
+            abercrombie.paintProbe(x,y)
+
+
 # Create the Menu
 name = "Abercrombie (#{version})"
 menu =
     "Start": -> imagejs.msg "You started something good."
+    "Place Probe": -> abercrombie.placeProbe()
 jmat.gId("menu").appendChild imagejs.menu menu, name
