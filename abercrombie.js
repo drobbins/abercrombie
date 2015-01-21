f856df5d2c7b67a2e5ad3ee07ad7b3d67396d03a
David Robbins <robbins.david@gmail.com>
2015-01-21 14:13:35 -0600

    Initial imagejs interaction code

--- abercrombie.js
+++ abercrombie.js
@@ -1,4 +1,20 @@
 (function() {
-  console.log("Abercrombie Loaded");
+  var id, menu, name, version;
+
+  version = "0.0.1";
+
+  id = "abercrombie";
+
+  imagejs.msg("" + id + " version " + version + " loaded.");
+
+  name = "Abercrombie (" + version + ")";
+
+  menu = {
+    "Start": function() {
+      return imagejs.msg("You started something good.");
+    }
+  };
+
+  jmat.gId("menu").appendChild(imagejs.menu(menu, name));
 
 }).call(this);
--- src/abercrombie.coffee
+++ src/abercrombie.coffee
@@ -1,1 +1,12 @@
-console.log "Abercrombie Loaded"
+# About this Module
+version = "0.0.1"
+id      = "abercrombie"
+
+# Say Hello
+imagejs.msg "#{id} version #{version} loaded."
+
+# Create the Menu
+name = "Abercrombie (#{version})"
+menu =
+    "Start": -> imagejs.msg "You started something good."
+jmat.gId("menu").appendChild imagejs.menu menu, name
